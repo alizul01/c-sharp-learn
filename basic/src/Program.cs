728b74f6f06d458f5377d117d52fd29549b939d6
alizul01 <alizulfikar032@gmail.com>
2023-03-19 06:28:33 +0700

    exception handling

--- basic/src/Program.cs
+++ basic/src/Program.cs
@@ -17,9 +17,12 @@
             Console.WriteLine("What is your age?"); // print
             int? age = Convert.ToInt32(Console.ReadLine()); // read input from user and convert to int
 
-            if (age < 17) {
+            if (age < 17)
+            {
                 Console.WriteLine("You are not allowed to vote."); // print
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("You are allowed to vote."); // print
             }
 
@@ -27,7 +30,8 @@
             Console.WriteLine("Learn about C# Switch Statement");
             Console.WriteLine("What is your favorite color?"); // print
             string? color = Console.ReadLine(); // read input from user
-            switch(color) {
+            switch (color)
+            {
                 case "red":
                     Console.WriteLine("Red is a nice color."); // print
                     break;
@@ -57,17 +61,44 @@
             Console.WriteLine(ran.Next(1, 10)); // print
 
             // Hypotenuse of a right triangle
-            Console.WriteLine("Enter side A : ");
-            double a = Convert.ToDouble(Console.ReadLine()); // read input from user and convert to Double
+            double a = 0;
+            double b = 0;
+            double c = 0;
 
-            Console.WriteLine("Enter side B : ");
-            double b = Convert.ToDouble(Console.ReadLine()); // read input from user and convert to int
+            try
+            {
+                Console.WriteLine("Enter side A : ");
+                a = Convert.ToDouble(Console.ReadLine()); // read input from user and convert to Double
+                Console.WriteLine("Enter side B : ");
+                b = Convert.ToDouble(Console.ReadLine()); // read input from user and convert to Double
 
-            if (a == 0 || b == 0) {
-                Console.WriteLine("Please enter a valid number."); // print
-            } else {
-                double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-                Console.WriteLine("The hypotenuse of the right triangle is " + c + "."); // print
+                if (b == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero"); // throw an exception if b is zero
+                }
+
+                // divide
+                c = a / b;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message); // print
+                b = 1; // initialize b to a default value
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message); // print
+                c = Double.NaN; // set c to NaN (Not a Number) to indicate that the result is undefined
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); // print
+            }
+            finally
+            {
+                Console.WriteLine("Value of a: " + a);
+                Console.WriteLine("Value of b: " + b);
+                Console.WriteLine("Value of c: " + c);
             }
 
             // String method
@@ -80,7 +111,7 @@
             // Array
             Console.WriteLine("Array");
 
-            String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+            String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
 
             Console.WriteLine("For Loop");
             for (int i = 0; i < cars.Length; i++)
@@ -100,20 +131,38 @@
             MyMethod(); // call method
             Console.WriteLine(Multiply(2, 3)); // print
             Console.WriteLine(Multiply(2.5, 3.5)); // print
+
+            // Params
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(add(numbers)); // print
         }
 
-        static void MyMethod() {
+        static void MyMethod()
+        {
             Console.WriteLine("I just got executed!"); // print
         }
 
         // Method with parameters
-        static int Multiply(int x, int y) {
+        static int Multiply(int x, int y)
+        {
             return x * y;
         }
 
         // Overloading
-        static double Multiply(double x, double y) {
+        static double Multiply(double x, double y)
+        {
             return x * y;
+        }
+
+        // Params
+        static int add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            return sum;
         }
     }
 }
